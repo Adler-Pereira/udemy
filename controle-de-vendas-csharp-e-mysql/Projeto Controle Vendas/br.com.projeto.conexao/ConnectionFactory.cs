3ecd1ff7b358799c79cc49102f41246c939e9779
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-06-01 11:21:39 -0300

    (Aula08): criação da ConnectionString e classes ConnectionFactory

---
+++ controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.conexao/ConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Controle_Vendas.br.com.projeto.conexao
+{
+    public class ConnectionFactory
+    {
+        public MySqlConnection GetConnection()
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["dbvendas"].ConnectionString;
+
+            return new MySqlConnection(conexao);
+        }
+    }
+}
