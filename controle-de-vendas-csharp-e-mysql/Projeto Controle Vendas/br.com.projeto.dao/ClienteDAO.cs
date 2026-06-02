04ce86729eb342034a10048c510735f88fb6e850
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-06-02 09:58:04 -0300

    (Aula13): criação dos métodos AlterarCliente e ExcluirCliente

--- controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.dao/ClienteDAO.cs
+++ controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.dao/ClienteDAO.cs
@@ -81,5 +81,80 @@ namespace Projeto_Controle_Vendas.br.com.projeto.dao
             }
         }
         #endregion
+
+        #region AlterarCliente
+        public void alterarCliente(Cliente obj)
+        {
+            try
+            {
+                string sql = @"update tb_clientes set (nome=@nome,
+                                                       rg=@rg,
+                                                       cpf=@cpf,
+                                                       email=@email,
+                                                       telefone=@telefone,
+                                                       celular=@celular,
+                                                       cep=@cep,
+                                                       endereco=@endereco,
+                                                       numero=@numero,
+                                                       complemento=@complemento,
+                                                       bairro=@bairro,
+                                                       cidade=@cidade,
+                                                       estado=@estado)
+                               where id=@id";
+
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@id", obj.codigo);
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente alterado com sucesso!");
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+        }
+
+        #endregion
+
+        #region ExcluirCliente
+        public void excluirCliente(Cliente obj)
+        {
+            try
+            {
+                string sql = @"delete from tb_clientes
+                               where id=@id";
+
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@id", obj.codigo);
+
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente excluído com sucesso!");
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+        }
+
+        #endregion
     }
 }
