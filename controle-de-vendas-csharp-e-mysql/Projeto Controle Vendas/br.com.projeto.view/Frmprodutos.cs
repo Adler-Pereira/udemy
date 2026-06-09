f8f3db914c04a3f4d65f09a01f7ed3e8756832d8
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-06-09 10:34:44 -0300

    (Aula38): criação da tela de produtos

---
+++ controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.view/Frmprodutos.cs
@@ -0,0 +1,29 @@
+﻿using Projeto_Controle_Vendas.br.com.projeto.dao;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Controle_Vendas.br.com.projeto.view
+{
+    public partial class Frmprodutos : Form
+    {
+        public Frmprodutos()
+        {
+            InitializeComponent();
+        }
+
+        private void Frmprodutos_Load(object sender, EventArgs e)
+        {
+            FornecedorDAO f_dao = new FornecedorDAO();
+            cbfornecedor.DataSource = f_dao.listarFornecedores();
+            cbfornecedor.DisplayMember = "nome";
+            cbfornecedor.ValueMember = "id";
+        }
+    }
+}
