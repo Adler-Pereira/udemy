ac2dc03dbdd5acb2c0435faffd129e23b1a2fe59
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-06-11 09:55:34 -0300

    (Aula56): criação da tela de pagamentos

---
+++ controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.view/Frmpagamentos.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Projeto_Controle_Vendas.br.com.projeto.view
+{
+    partial class Frmpagamentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtdinheiro = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtcartao = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txttroco = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttotal = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnfinalizar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtdinheiro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtcartao)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txttroco)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txttotal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(72, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pagamento";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(468, 92);
+            this.panel1.TabIndex = 15;
+            // 
+            // txtdinheiro
+            // 
+            this.txtdinheiro.DecimalPlaces = 2;
+            this.txtdinheiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdinheiro.Location = new System.Drawing.Point(251, 130);
+            this.txtdinheiro.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txtdinheiro.Name = "txtdinheiro";
+            this.txtdinheiro.Size = new System.Drawing.Size(198, 47);
+            this.txtdinheiro.TabIndex = 36;
+            this.txtdinheiro.ThousandsSeparator = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(12, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 39);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Dinheiro (R$):";
+            // 
+            // txtcartao
+            // 
+            this.txtcartao.DecimalPlaces = 2;
+            this.txtcartao.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcartao.Location = new System.Drawing.Point(251, 196);
+            this.txtcartao.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txtcartao.Name = "txtcartao";
+            this.txtcartao.Size = new System.Drawing.Size(198, 47);
+            this.txtcartao.TabIndex = 38;
+            this.txtcartao.ThousandsSeparator = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(114, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 39);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Cartão:";
+            // 
+            // txttroco
+            // 
+            this.txttroco.DecimalPlaces = 2;
+            this.txttroco.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttroco.Location = new System.Drawing.Point(251, 263);
+            this.txttroco.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txttroco.Name = "txttroco";
+            this.txttroco.ReadOnly = true;
+            this.txttroco.Size = new System.Drawing.Size(198, 47);
+            this.txttroco.TabIndex = 40;
+            this.txttroco.ThousandsSeparator = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(129, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 39);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Troco:";
+            // 
+            // txttotal
+            // 
+            this.txttotal.DecimalPlaces = 2;
+            this.txttotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttotal.Location = new System.Drawing.Point(251, 331);
+            this.txttotal.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txttotal.Name = "txttotal";
+            this.txttotal.ReadOnly = true;
+            this.txttotal.Size = new System.Drawing.Size(198, 47);
+            this.txttotal.TabIndex = 42;
+            this.txttotal.ThousandsSeparator = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(140, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 39);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Total:";
+            // 
+            // btnfinalizar
+            // 
+            this.btnfinalizar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnfinalizar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnfinalizar.ForeColor = System.Drawing.Color.White;
+            this.btnfinalizar.Location = new System.Drawing.Point(19, 398);
+            this.btnfinalizar.Name = "btnfinalizar";
+            this.btnfinalizar.Size = new System.Drawing.Size(430, 60);
+            this.btnfinalizar.TabIndex = 43;
+            this.btnfinalizar.Text = "Finalizar Venda";
+            this.btnfinalizar.UseVisualStyleBackColor = false;
+            // 
+            // Frmpagamentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 485);
+            this.Controls.Add(this.btnfinalizar);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txttroco);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtcartao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtdinheiro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Name = "Frmpagamentos";
+            this.Text = "Tela Pagamentos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtdinheiro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtcartao)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txttroco)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txttotal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown txtdinheiro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown txtcartao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown txttroco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnfinalizar;
+        public System.Windows.Forms.NumericUpDown txttotal;
+    }
+}
