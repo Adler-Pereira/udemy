5b0a89af268b8bbb6a00ce3ed5f6744efcf8a183
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-06-11 12:24:46 -0300

    (Aula61): criação da classe ItemVendaDAO e o método CadastrarItem

---
+++ controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.dao/ItemVendaDAO.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_Controle_Vendas.br.com.projeto.conexao;
+using Projeto_Controle_Vendas.br.com.projeto.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Controle_Vendas.br.com.projeto.dao
+{
+    public class ItemVendaDAO
+    {
+        private MySqlConnection conexao;
+
+        public ItemVendaDAO()
+        {
+            this.conexao = new ConnectionFactory().GetConnection();
+        }
+
+        #region CadastrarItem
+        public void cadastrarItem(ItemVenda obj)
+        {
+            try
+            {
+                string sql = @"INSERT INTO tb_itensvendas (venda_id, produto_id, qtd, subtotal)
+                               VALUES (@venda_id, @produto_id, @qtd, @subtotal)";
+
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@venda_id", obj.venda_id);
+                executacmd.Parameters.AddWithValue("@produto_id", obj.produto_id);
+                executacmd.Parameters.AddWithValue("@qtd", obj.qtd);
+                executacmd.Parameters.AddWithValue("@subtotal", obj.subtotal);
+
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Item cadastrado com Sucesso!");
+                conexao.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+        }
+        #endregion
+    }
+}
