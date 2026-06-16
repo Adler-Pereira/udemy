eaee32c2a6b8c7cf416c8928d1e228a1fca7db83
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-06-16 09:01:02 -0300

    (Aula72): criação do método ListarItensPorVenda

--- controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.dao/ItemVendaDAO.cs
+++ controle-de-vendas-csharp-e-mysql/Projeto Controle Vendas/br.com.projeto.dao/ItemVendaDAO.cs
@@ -3,6 +3,7 @@ using Projeto_Controle_Vendas.br.com.projeto.conexao;
 using Projeto_Controle_Vendas.br.com.projeto.model;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,6 +44,43 @@ namespace Projeto_Controle_Vendas.br.com.projeto.dao
             catch (Exception erro)
             {
                 MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+        }
+        #endregion
+
+        #region ListarItensPorVenda
+        public DataTable listarItensPorVenda(int vendaId)
+        {
+            try
+            {
+                DataTable tabelaItensVendas = new DataTable();
+                string sql = @"SELECT
+                                   iv.id AS 'Código',
+                                   p.descricao AS 'Descrição',
+                                   iv.qtd AS 'Quantidade',
+                                   p.preco AS 'Preço',
+                                   iv.subtotal AS 'Subtotal'
+                               FROM tb_itensvendas iv JOIN tb_produtos p ON (iv.produto_id=p.id)
+                               WHERE venda_id=@vendaId";
+
+                MySqlCommand executacmd = new MySqlCommand(sql, conexao);
+
+                executacmd.Parameters.AddWithValue("@vendaId", vendaId);
+
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(executacmd);
+                da.Fill(tabelaItensVendas);
+
+                conexao.Close();
+
+                return tabelaItensVendas;
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao executar o comando sql: " + erro);
+                return null;
             }
         }
         #endregion
